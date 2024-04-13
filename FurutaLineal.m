8092c3e9e0d129113cd9a6cac4e663a49561678c
shredia <36351492+shredia@users.noreply.github.com>
2024-04-12 21:39:42 -0400

    Se modificó el lineal

--- FurutaLineal.m
+++ FurutaLineal.m
@@ -10,8 +10,8 @@ function setup(block)
     block.NumDialogPrms = 0;
 
     %% Register number of input and output ports
-    block.NumInputPorts  = 3;
-    block.NumOutputPorts = 4;
+    block.NumInputPorts  = 1;
+    block.NumOutputPorts = 5;
 
     %% Setup functional port properties to dynamically
     %% inherited.
@@ -20,23 +20,21 @@ function setup(block)
 
     block.InputPort(1).Dimensions        = 1;
     block.InputPort(1).DirectFeedthrough = false;
-    block.InputPort(2).Dimensions        = 1;
-    block.InputPort(2).DirectFeedthrough = false;
-    block.InputPort(3).Dimensions        = 1;
-    block.InputPort(3).DirectFeedthrough = false;
+    
 
 
     block.OutputPort(1).Dimensions       = 1;
     block.OutputPort(2).Dimensions       = 1;
     block.OutputPort(3).Dimensions       = 1;
     block.OutputPort(4).Dimensions       = 1;
+    block.OutputPort(5).Dimensions       = 1;
     
 
     %% Set block sample time to continuous
     block.SampleTimes = [0 0];
 
     
-    block.NumContStates = 4;
+    block.NumContStates = 5;
 
     %% Set the block simStateCompliance to default (i.e., same as a built-in block)
     block.SimStateCompliance = 'DefaultSimState';
@@ -56,6 +54,7 @@ function InitConditions(block)
     block.ContStates.Data(2) = 0;   % Phi
     block.ContStates.Data(3) = 0;   %dTheta
     block.ContStates.Data(4) = 0;   %dPhi
+    block.ContStates.Data(5) = 0;   %Corriente
 end
 
 
@@ -65,6 +64,8 @@ function Output(block)
     block.OutputPort(2).Data = block.ContStates.Data(2); %Phi
     block.OutputPort(3).Data = block.ContStates.Data(3); %dTheta
     block.OutputPort(4).Data = block.ContStates.Data(4); %dPhi
+    block.OutputPort(4).Data = block.ContStates.Data(5); %Corriente
+
     
 end
 
@@ -79,58 +80,21 @@ function Derivative(block)
     C_z = 4.4;      % Valor de C_z
     I_x = 4.39e-4;  % Valor de I_x
     I_z = 1.88e-4;  % Valor de I_z
+    K = 1.32;
+    R = 9.3;
+    L = 43.1;
     
 
     % Entradas
-    Tau = block.InputPort(1).Data;
-    Tau_fu = block.InputPort(2).Data;
-    Tau_fp = block.InputPort(3).Data;
-
-    d = (J + 2*M_2*l_bi*C_x + M_2*l_bi^2 + I_z)*I_x - M_2^2*l_bi^2*C_z^2;
-    B11 = 0;
-    B21 = 0;
-    B31 = M_2*l_bi*C_z/d;
-    B41 = I_x/d;
-
-    B12 = 0;
-    B22 = 0;
-    B32 = 0;
-    B42 = 0;
-
-    % Matriz U
-    B = [B11 B12; B21 B22; B31 B32; B41 B42];
-
-    % Matriz A
-    A11 = 0;
-    A21 = 0;
-    A31 = M_2*g*C_z*(J+2*M_2*l_bi*C_x+M_2*l_bi^2+I_z)/d;
-    A41 = M_2^2*C_z^2*l_bi*g/d;
-
-    A12 = 0;
-    A22 = 0;
-    A32 = 0;
-    A42 = 0;
-
-    A13 = 1;
-    A23 = 0;
-    A33 = 0;
-    A43 = 0;
-
-    A14 = 0;
-    A24 = 1;
-    A34 = 0;
-    A44 = 0;
-
-    A = [A11 A12 A13 A14; A21 A22 A23 A24; A31 A32 A33 A34; A41 A42 A43 A44];
-
+    
+    A = [0, 0, 1, 0, 0; 0, 0, 0, 1, 0; 0, (C_z^2*M_2^2*g*l_bi)/(- C_z^2*M_2^2*l_bi^2 + I_x*M_2*l_bi^2 + 2*C_x*I_x*M_2*l_bi + I_x*I_z + I_x*J), 0, 0, I_x/(- C_z^2*M_2^2*l_bi^2 + I_x*M_2*l_bi^2 + 2*C_x*I_x*M_2*l_bi + I_x*I_z + I_x*J); 0, -(C_z*M_2^2*g*l_bi^2 + C_z*I_z*M_2*g + C_z*J*M_2*g + 2*C_x*C_z*M_2^2*g*l_bi)/(- C_z^2*M_2^2*l_bi^2 + I_x*M_2*l_bi^2 + 2*C_x*I_x*M_2*l_bi + I_x*I_z + I_x*J), 0, 0, (C_z*M_2*l_bi)/(- C_z^2*M_2^2*l_bi^2 + I_x*M_2*l_bi^2 + 2*C_x*I_x*M_2*l_bi + I_x*I_z + I_x*J); 0, 0, 0, K/L, -R/L];
    
     
 
     % Calcular las derivadas
-    x = [block.ContStates.Data(1); block.ContStates.Data(2); block.ContStates.Data(3); block.ContStates.Data(4)];
-    U = [Tau + Tau_fu; Tau_fp];
+    x = [block.ContStates.Data(1); block.ContStates.Data(2); block.ContStates.Data(3); block.ContStates.Data(4); block.ContStates.Data(5)];  % 
+    U = block.InputPort(1).Data;  % 
+    B = [0;0;0;0;1];
     dx_dt = A * x + B * U;
     block.Derivatives.Data = dx_dt;
-    
-    
 end
--- FurutanolinealSLV2.m
+++ FurutanolinealSLV2.m
@@ -13,8 +13,8 @@ function setup(block)
   block.NumDialogPrms = 0;
 
   %% Registrar el número de puertos de entrada y salida
-  block.NumInputPorts  = 3;
-  block.NumOutputPorts = 4;
+  block.NumInputPorts  = 1;
+  block.NumOutputPorts = 5;
 
   %% Configurar propiedades de puerto funcional para heredar dinámicamente
   block.SetPreCompInpPortInfoToDynamic;
@@ -22,11 +22,7 @@ function setup(block)
 
   block.InputPort(1).Dimensions        = 1;
   block.InputPort(1).DirectFeedthrough = false;
-  block.InputPort(2).Dimensions        = 1;
-  block.InputPort(2).DirectFeedthrough = false;
-  block.InputPort(3).Dimensions        = 1;
-  block.InputPort(3).DirectFeedthrough = false;
-
+  
   block.OutputPort(1).Dimensions       = 1;
    block.OutputPort(2).Dimensions       = 1;
     block.OutputPort(3).Dimensions       = 1;
@@ -36,7 +32,7 @@ function setup(block)
   block.SampleTimes = [0 0];
 
   %% Configurar Dwork
-  block.NumContStates = 4;
+  block.NumContStates = 5;
 
   %% Establecer la conformidad del estado de simulación del bloque a predeterminado (es decir, igual que un bloque integrado)
   block.SimStateCompliance = 'DefaultSimState';
@@ -50,10 +46,12 @@ end
 
 function InitConditions(block)
   % Inicializar Dwork
-    block.ContStates.Data(1) = 0;  % Estado 1
-    block.ContStates.Data(2) = pi; % Estado 2
-    block.ContStates.Data(3) = 0;  % Estado 3
-    block.ContStates.Data(4) = 0;  % Estado 4
+       
+    block.ContStates.Data(1) = pi;  % Theta
+    block.ContStates.Data(2) = 0;   % Phi
+    block.ContStates.Data(3) = 0;   %dTheta
+    block.ContStates.Data(4) = 0;   %dPhi
+    block.ContStates.Data(5) = 0;   %Corriente
   
 end
 
@@ -62,26 +60,28 @@ function Output(block)
   block.OutputPort(2).Data = block.ContStates.Data(2);
   block.OutputPort(3).Data = block.ContStates.Data(3);
   block.OutputPort(4).Data = block.ContStates.Data(4);
+  block.OutputPort(5).Data = block.ContStates.Data(5);
 end
 
 function Derivative(block)
-  % Definir las constantes
-  g = 9.81;       % Valor de la gravedad
-  
-  
-  J = 0.0321;     % Valor de J
-  M_2 = 98/1000;       % Valor de M_2
-  l_bi = 8.7;     % Valor de l_bi %%falta dividir por 100 pero se indefine
-  C_x = -4/100;     % Valor de C_x
-  C_z = 4.4;      % Valor de C_z %es 4.4/100, pero se indefine
-  I_x = 4.39e-4;  % Valor de I_x
-  I_z = 2.24e-4;  % Valor de I_z
-  
+ % Definición de constantes
+    g = 9.81;       % Valor de la gravedad
+           
+    J = 0.0321;     % Valor de J
+    M_2 = 98/1000;       % Valor de M_2
+    l_bi = 8.7;     % Valor de l_bi
+    C_x = -4/100;     % Valor de C_x
+    C_z = 4.4;      % Valor de C_z
+    I_x = 4.39e-4;  % Valor de I_x
+    I_z = 1.88e-4;  % Valor de I_z
+    K = 1.32;
+    R = 9.3;
+    L = 43.1;
+    
 
   % Entradas
-  Tau = block.InputPort(1).Data;
-  Tau_fu = block.InputPort(2).Data;
-  Tau_fp = block.InputPort(3).Data;
+  V = block.InputPort(1).Data;
+ 
 
   % Ecuaciones de U
  
@@ -119,7 +119,8 @@ function Derivative(block)
   F = [Ftheta1_1; Ftheta1_2];
 
   % Derivadas
-  dx = [block.ContStates.Data(4); block.ContStates.Data(3); inv(M) * (U - D * Qp - F)];
+  dx = [block.ContStates.Data(4); block.ContStates.Data(3); M\(U - D * Qp - F)];
+  dx = []
 
   % Asignar derivadas al bloque
   block.Derivatives.Data(1) = dx(1);
